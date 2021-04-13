8c119ff645f2c5aab2c5539031b20fe288fce6cf
Dmytro Panchenko <lerthe61@ukr.net>
2021-04-13 16:34:58 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# reveal.estimates.for.Azure.DevOps.estimate.boards.js
+reveal.estimates.for.Azure.DevOps.estimate.boards.js
+===============
+
 Tampermonkey script that will reveal estimates on Azure DevOps estimation board
+
+## prerequisites
+ 
+ - greasemonkey addon for your browser
+ - chrome: [tampermonkey extension](https://chrome.google.com/webstore/detail/tampermonkey/dhdgffkkebhmkfjojejmpbldmpobfkfo?hl=en)
+
+## installation
+
+ - install the appropriate addon for your browser
+ - open the [reveal.estimates.for.Azure.DevOps.estimate.boards.js](reveal.estimates.for.Azure.DevOps.estimate.boards.js?raw=true) script in your browser and click the install button.
+ 
+ ## usage
+
+ - open DevOps estimation board
+ - start estimation session
+ - wait till the first estimates
+
+![Screenshot](screenshot.png?raw=true "Screenshot of the UI")
---
+++ reveal.estimates.for.Azure.DevOps.estimate.boards.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         reveal.estimates.for.Azure.DevOps.estimate.boards.js
+// @namespace    https://github.com/lerthe61/reveal.estimates.for.Azure.DevOps.estimate.boards.js
+// @version      0.1
+// @description  Revealing other votes on Estimate Board in Azure DevOps
+// @author       lerthe61
+// @match        https://ms-devlabs.gallerycdn.vsassets.io/extensions/ms-devlabs/estimate/*/build/index.html
+// @require      https://code.jquery.com/jquery-3.4.1.slim.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+     /* globals jQuery */
+    var $_new = jQuery.noConflict(true);
+
+    function flipOne(node) {
+        if (!node.classList.contains('vote-container')) { return; }
+
+        var backValue = node.querySelector('.card--back').innerText;
+        var frontNode = node.querySelector('.card--front');
+        // Put 'hidden' score in parentheses
+        frontNode.innerText = '(' + backValue +')';
+    };
+
+    function flipAll() {
+        let list = window.document.querySelectorAll("div.vote-container");
+        list.forEach(flipOne);
+     };
+
+    function watcher(){
+        let list = window.document.querySelectorAll("div.vote-container");
+        if (!NodeList.prototype.isPrototypeOf(list)) {
+            window.setTimeout(watcher, 500);
+        } else {
+            flipAll();
+            window.setTimeout(watcher, 500);
+        }
+    };
+    watcher();
+})();
